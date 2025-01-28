d954a0ae22ac72436db34d423cccd70680d1283d
Jirch Tumbokon <jirchgot@gmail.com>
2025-01-28 09:43:53 -0700

    Created Employee class

--- LabInheritance/Employee.cs
+++ LabInheritance/Employee.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace LabInheritance
 {
-    internal class Employee
+    public class Employee
     {
+        private string id;
+        private string name;
+        private string address;
+
+        //public Employee() { }
+
+        public string Id { get { return id; } }
+        public string Name { get { return name; } }
+        public string Address { get { return address; } }
+        public Employee(string id, string name, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
     }
 }
